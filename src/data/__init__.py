c88441be448db565ff12fa686069aa8bbe72deb3
Marcos Achaval <marcos.achavalr@gmail.com>
2025-07-18 18:03:57 -0300

    Remove QuestDBManager and related imports from data management components

--- src/data/__init__.py
+++ src/data/__init__.py
@@ -1,13 +1,11 @@
 """Data management components"""
 
-from .storage.database import QuestDBManager
 from .storage.file_manager import FileManager
 from .config.data_config import DataConfig, DataSplitConfig
 from .config.data_validator import DataValidator
 from .pipeline.data_preparation import DataPreparationPipeline
 
 __all__ = [
-    'QuestDBManager',
     'FileManager',
     'DataConfig',
     'DataSplitConfig',
--- src/data/storage/__init__.py
+++ src/data/storage/__init__.py
@@ -1,9 +1,7 @@
 """Data storage components"""
 
-from .database import QuestDBManager
 from .file_manager import FileManager
 
 __all__ = [
-    'QuestDBManager',
     'FileManager'
 ]
